d16ee41d91f94541815a6b915f848fbbaa355080
Thunder-ace <125556072+Thunder-ace@users.noreply.github.com>
2023-02-16 11:46:33 +0100

    Add files via upload

---
+++ gas.py
@@ -0,0 +1,33 @@
+import asyncio
+import datetime
+from energyzero import EnergyZero
+
+async def main() -> None:
+    async with EnergyZero() as client:
+        local        = pytz.timezone("Europe/Amsterdam")
+        today        = datetime.datetime.now().date()
+        gas_today    = None
+        # get energy prices today, when none go to date-1 and try again until result
+        while gas_today is None:
+            try:
+                gas_today      = await client.gas_prices(start_date=today, end_date=today)
+                # print average price ex and incl.
+                print(gas_today.average_price)
+                print(round(gas_today.average_price + 0.67541, 2))
+                # make record for json file
+                #result = {
+                #    "1" : today.strftime('%d-%m-%Y'),
+                #    "2" : ("{:.2f}".format(gas_today.average_price)),
+                #    "3" : ("{:.2f}".format(gas_today.average_price + 0.67541))
+                #}
+            except:
+                gas_today = None
+                today     = today - datetime.timedelta(days=1)
+            pass
+        # save json record
+        # with open('/home/rcbleeker/conky/gas.json', 'w') as f:
+        #         json.dump(result, f)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ stroom.py
@@ -0,0 +1,35 @@
+import asyncio
+import datetime
+#import json
+import pytz
+from energyzero import EnergyZero
+
+async def main() -> None:
+    async with EnergyZero() as client:
+        local        = pytz.timezone("Europe/Amsterdam")
+        today        = datetime.datetime.now().date()
+        energy_today = None
+        # get energy prices today, when none go to date-1 and try again until result
+        while energy_today is None:
+            try:
+                energy_today    = await client.energy_prices(start_date=today, end_date=today)
+                # print average price ex and incl.
+                print(energy_today.average_price)
+                print(round(energy_today.average_price + 0.17364, 2))
+                # make record for json file
+                #result = {
+                #    "1" : today.strftime('%d-%m-%Y'),
+                #    "2" : ("{:.2f}".format(energy_today.average_price)),
+                #    "3" : ("{:.2f}".format(energy_today.average_price + 0.17364))
+                #}
+            except:
+                gas_today = None
+                today     = today - datetime.timedelta(days=1)
+            pass 
+        # save json record       
+        #with open('/home/rcbleeker/conky/stroom.json', 'w') as f:
+        #      	json.dump(result, f)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
