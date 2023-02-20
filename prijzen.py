538388e6f77728b89d7e3f5e1b8b61a201ba4915
J.v.M <125556072+Thunder-ace@users.noreply.github.com>
2023-02-20 12:09:40 +0100

    updated more details in logfile and better error handeling

--- prijzen.py
+++ prijzen.py
@@ -6,90 +6,121 @@ import sqlite3
 # Energyzero module : pip install energyzero
 from energyzero import EnergyZero
 
+
 wan_connected = False
-# Check of internet bereikbaar is
-while not wan_connected:
+MAX_ATTEMPTS  = 5
+attempts      = 0
+# Check for internet connection, if not found retry 5 times after a wait for 60 seconds : no change when no connection.
+while not wan_connected and attempts < MAX_ATTEMPTS:
     try:
         ping_output = subprocess.check_output(['ping', '-4', '-c', '1', '-w', '2', '208.67.222.222'])
         if b'1 received' in ping_output:
-            print()
-            print('Internet bereikbaar !')
-            print()
-            break
+            print('----------------------------------------------------------------------')
+            print('Internet bereikbaar.')
+            print('----------------------------------------------------------------------')
+            wan_connected = True
         else:
-            print('Fout, internet niet bereikbaar !')
-            time.sleep(60)
+            attempts += 1
+            if attempts < MAX_ATTEMPTS:
+                print(f'Nieuwe poging over 60 seconden (poging {attempts} van {MAX_ATTEMPTS})')
+                time.sleep(60)
     except subprocess.CalledProcessError as d:
-        print(f'Ping request failed with error: Netwerk is onbereikbaar')
-        print('Nieuwe poging over 60 seconden !')
-        time.sleep(60)
+        print('----------------------------------------------------------------------')
+        print('Internet niet bereikbaar !')
+        attempts += 1
+        if attempts < MAX_ATTEMPTS:
+            print(f'Nieuwe poging over 60 seconden (poging : {attempts} van {MAX_ATTEMPTS})')
+            time.sleep(60)
+        else:
+            check_wan = datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
+            print('----------------------------------------------------------------------')
+            print(f'Datum en tijd : {check_wan}')
+            print(f'Energyzero api via internet niet bereikbaar, aantal pogingen {MAX_ATTEMPTS}.')
+            print('Geen wijzigingen van de actuele Energyzero prijzen voor stroom en gas.')
+            print('----------------------------------------------------------------------')
+            exit()
 
 
 async def main() -> None:
     async with EnergyZero() as client:
-        today            = datetime.datetime.now().date()
-        energy_today     = None
-        gas_today        = None
-        current_electric = None
-        current_gas      = None
+        today              = datetime.datetime.now().date()
+        energy_today       = None
+        gas_today          = None
+        # electricity price
+        current_electric   = None
+        # electricity surcharges (change if needed)
+        inkoopkosten_e     = 0.02118
+        energiebelasting_e = 0.15246
+        ode_e              = 0.00
+        # gas price
+        current_gas        = None
+        # gas surcharges (change if needed)
+        inkoopkosten_g     = 0.08275
+        energiebelasting_g = 0.59266
+        ode_g              = 0.00
 
-        # get energy and gas prices for today, when none go to date-1 and try again until result
-        while energy_today is None or gas_today is None:
-            try:
-                while energy_today is None or gas_today is None:
-                    try:
-                        if energy_today is None:
-                            energy_today = await client.energy_prices(start_date=today, end_date=today)
-                            if energy_today.current_price is None:
-                                current_electric = energy_today.average_price  # set default price here
-                            else:
-                                current_electric = energy_today.current_price
-
-                        if gas_today is None:
-                            gas_today = await client.gas_prices(start_date=today, end_date=today)
-                            if gas_today.current_price is None:
-                                current_gas = gas_today.average_price  # set default price here
-                            else:
-                                current_gas = gas_today.current_price
-                    except Exception as e:
-                        print('Error : fout bij ophalen Energyzero prijzen')
-                        print(e.message, e.args)
-
-                # print Energyzero electric price
-                # print(current_electric)
-                # print(f"Actuele stroom prijs    incl. BTW : {energy_today.current_price}")
-                # print(f"Gemiddelde stroom prijs incl. BTW : {energy_today.average_price}")
-                # print()
-                # print Energyzero gas price
-                # print(current_gas)
-                # print()
-                # print(f"Actuele gas prijs    incl. BTW : {gas_today.current_price}")
-                # print(f"Gemiddelde gas prijs incl. BTW : {gas_today.average_price}")
-                # print()
-            except Exception as f:
-                energy_today = None
-                gas_today    = None
-                today        = today - datetime.timedelta(days=1)
-                print('Error : fout bij ophalen Energyzero prijzen')
-                print(f.message, f.args)
-                exit()
-
+        # get actual price(s) for electricity, when none found quite update
         try:
-            replace_electric = round(current_electric + 0.17364, 5)     # add inkoopkosten, energiebelasting, ODE
-            replace_solar    = round(current_electric, 5)               # ex inkoopkosten, energiebelasting, ODE
-            replace_gas      = round(current_gas + 0.67541, 5)          # add inkoopkosten, energiebelasting, ODE
-            set_price(replace_electric, replace_solar, replace_gas)
-            print()
-            print(' Energyzero prijzen -----------------')
-            print()
-            print(' Stroom prijs all inclusief : ' + str(replace_electric))
-            print(' Prijs teruglevering        : ' + str(replace_solar))
-            print(' Gas prijs all inclusief    : ' + str(replace_gas))
-            print(' ------------------------------------')
-        except Exception as g:
-            print('Er is een fout opgetreden bij het updaten de Energyzero prijzen.')
-            print(g)
+            energy_today = await client.energy_prices(start_date=today, end_date=today)
+            if energy_today is None or energy_today.current_price is None:
+                print('Geen actuele prijs voor Energyzero stroom gevonden.')
+                print('Geen updates voor Energyzero stroom verwerkt.')
+                exit()  # or raise an exception
+            else:
+                # Set Energyzero current price electric
+                current_electric = energy_today.current_price
+                # print('----------------------------------------------------------------------')
+                # print(f'Actuele stroom prijs    incl. BTW : € {current_electric}')
+                # print(f'Gemiddelde stroom prijs incl. BTW : € {energy_today.average_price}')
+                # print('----------------------------------------------------------------------')
+        except Exception as e_stroom:
+            print('Er is een fout opgetreden bij het ophalen van Energyzero prijs voor stroom.')
+            print(f'Details :', e_stroom)
             exit()
+
+        # get actual price(s) for gas, when none found quite update
+        try:
+            gas_today = await client.gas_prices(start_date=today, end_date=today)
+            if gas_today is None or gas_today.current_price is None:
+                print('Geen actuele prijs voor Energyzero gas gevonden.')
+                print('Geen updates voor Energyzero gas verwerkt.')
+                exit()  # or raise an exception
+            else:
+                # Set Energyzero current price electric
+                current_gas = gas_today.current_price
+                # print(f'Actuele gas prijs    incl. BTW    : € {current_gas}')
+                # print(f'Gemiddelde gas prijs incl. BTW    : € {gas_today.average_price}')
+                # print('----------------------------------------------------------------------')
+        except Exception as e_gas:
+            print('Er is een fout opgetreden bij het ophalen van Energyzero prijs voor gas.')
+            print(f'Details :', e_gas)
+            exit()
+
+
+    try:
+        replace_electric = round(current_electric + inkoopkosten_e + energiebelasting_e + ode_e, 5)
+        replace_solar    = round(current_electric, 5)
+        replace_gas      = round(current_gas + inkoopkosten_g + energiebelasting_g + ode_g, 5)
+        set_price(replace_electric, replace_solar, replace_gas)
+        print(f'Stroom prijs                               : € {current_electric}')
+        print(f'Inkoopkosten                               : € {inkoopkosten_e}')
+        print(f'Energiebelasting                           : € {energiebelasting_e}')
+        print(f'ODE                                        : € {ode_e}')
+        print(f'Stroom prijs inclusief                     : € {replace_electric}')
+        print('----------------------------------------------------------------------')
+        print(f'Prijs teruglevering                        : € {current_electric} ')
+        print('----------------------------------------------------------------------')
+        print(f'Gas prijs                                  : € {current_gas}')
+        print(f'Inkoopkosten                               : € {inkoopkosten_g}')
+        print(f'Energiebelasting                           : € {energiebelasting_g}')
+        print(f'ODE                                        : € {ode_g}')
+        print(f'Gas prijs inclusief                        : € {replace_gas}')
+        print('----------------------------------------------------------------------')
+    except Exception as g_stroom:
+        print()
+        print('Er is een fout opgetreden bij het updaten de Energyzero prijzen.')
+        print(f'Details :', g_stroom)
+        exit()
 
 
 def set_price(replace_electric, replace_solar, replace_gas):
@@ -115,13 +146,40 @@ def set_price(replace_electric, replace_solar, replace_gas):
         cursor.execute(sql_update_query_15)  # update de config.db database
         sqlite_connection.commit()           # Sla de database aanpassingen op
         sqlite_connection.close()            # Sluit de database
-        today = datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
-        print('Update Energyzero gelukt op : ' + today)
-        print("Succes : config.db geüpdatet met Energyzero prijzen.")
-
     except Exception as h:
-        print("Fout bij het updaten van ~/p1mon/mnt/ramdisk/config.db met de Energyzero prijzen.")
+        print("Fout bij het updaten van /p1mon/mnt/ramdisk/config.db met de Energyzero prijzen.")
         print(h)
+
+    try:
+        sqlite_connection = sqlite3.connect("/p1mon/data/config.db")  # connect with config.db
+        cursor           = sqlite_connection.cursor()
+        # update the old prices with the new Energyzero prices
+        sql_update_query_01 = """update config set PARAMETER='""" + str(replace_electric) + """',LABEL='Verbruik tarief 
+        elektriciteit dal in euro.' where ID='1';"""
+        sql_update_query_02 = """update config set PARAMETER='""" + str(replace_electric) + """',LABEL='Verbruik tarief 
+        elektriciteit piek in euro.' where ID='2';"""
+        sql_update_query_03 = """update config set PARAMETER='""" + str(replace_solar) + """',LABEL='Geleverd tarief 
+        elektriciteit dal in euro.' where ID='3';"""
+        sql_update_query_04 = """update config set PARAMETER='""" + str(replace_solar) + """',LABEL='Geleverd tarief 
+        elektriciteit piek in euro.' where ID='4';"""
+        sql_update_query_15 = """update config set PARAMETER='""" + str(replace_gas) + """',LABEL='Verbruik tarief 
+        gas in euro.' where ID='15';"""
+        cursor.execute(sql_update_query_01)  # update de config.db database
+        cursor.execute(sql_update_query_02)  # update de config.db database
+        cursor.execute(sql_update_query_03)  # update de config.db database
+        cursor.execute(sql_update_query_04)  # update de config.db database
+        cursor.execute(sql_update_query_15)  # update de config.db database
+        sqlite_connection.commit()           # Sla de database aanpassingen op
+        sqlite_connection.close()            # Sluit de database
+
+        today = datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S")
+        print('Update Energyzero gelukt op                : ' + today)
+        print("Config.db geüpdatet met Energyzero prijzen : Succes .")
+        print('----------------------------------------------------------------------')
+
+    except Exception as i:
+        print("Fout bij het updaten van /p1mon/data/config.db met de Energyzero prijzen.")
+        print(i)
 
 
 if __name__ == "__main__":
